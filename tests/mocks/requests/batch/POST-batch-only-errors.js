2b97568869bcf4df1d26c978ac37a6581187207b
adamz0210 <adamz0210@yahoo.com>
2023-08-19 17:53:26 -0400

    update mocks

---
+++ tests/mocks/requests/batch/POST-batch-only-errors.js
@@ -0,0 +1,19 @@
+const convert = require('xml-js');
+
+const responseData = `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type='text/xsl' href='/obix/xsl'?>
+<list of="obix:BatchOut" xmlns="http://obix.org/ns/schema/1.0" xsi:schemaLocation="http://obix.org/ns/schema/1.0 /obix/xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <err href="https://localhost:443/obix/config/Test/BooleanWritable2/out" is="obix:BadUriErr" display="/Test/BooleanWritable2/out"/>
+    <err href="https://localhost:443/obix/config/Test/NumericWritable1/set" is="obix:BadUriErr" display="/Test/NumericWritable1/set"/>
+</list>`;
+
+const responseConverted = convert.xml2js(responseData, { compact: true, spaces: 4 });
+const payload = `<list is="obix:BatchIn">
+<uri is="obix:Read" val="https://localhost:443/obix/config/Test/BooleanWritable2/out" >
+</uri>
+<uri is="obix:Invoke" val="https://localhost:443/obix/config/Test/NumericWritable1/set">
+    <real name="in" val="50" />
+</uri>
+</list>`;
+
+module.exports = { postBatchResponse: responseConverted, postBatchPayload: payload };
